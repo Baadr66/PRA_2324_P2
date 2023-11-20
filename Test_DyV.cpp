b8d777a6fe4c6619c85c52ec42fb76c474548745
Baadr66 <145703927+Baadr66@users.noreply.github.com>
2023-11-20 13:44:36 +0100

    Update Test_DyV.cpp

--- Test_DyV.cpp
+++ Test_DyV.cpp
@@ -1,6 +1,7 @@
 #include "DyV.h"
 #include <iostream>
 #include <stdio.h>
+#include<chrono>
 using namespace std;
 int main (){
         int V[7] = {1,3,6,9,19,44,55};
@@ -9,6 +10,9 @@ int main (){
         auto start = std::chrono::system_clock::now();
         QuickSort(V, ini,fin);
         Partition(V, ini, fin);
+        auto end = std::chrono::system_clock::now();
+        std::chrono::duration<float, std::milli> duration = end - start;
+        std::cout << “Time: “ << duration.count() << “s” << std::endl;
 
 
 }
