b9deea58fb87bf407d460112d9c6bf360bb5b6da
Baadr66 <145703927+Baadr66@users.noreply.github.com>
2023-11-20 13:37:42 +0100

    Update Test_DyV.cpp

--- Test_DyV.cpp
+++ Test_DyV.cpp
@@ -6,6 +6,7 @@ int main (){
         int V[7] = {1,3,6,9,19,44,55};
         int ini=0;
         int fin =7;
+        auto start = std::chrono::system_clock::now();
         QuickSort(V, ini,fin);
         Partition(V, ini, fin);
 
