38357c6cc5d285258fb95c22f9175a78d5a56f91
Baadr66 <145703927+Baadr66@users.noreply.github.com>
2023-11-20 14:24:17 +0100

    QuickSort.h

--- QuickSort.h
+++ QuickSort.h
@@ -1,27 +1,41 @@
-#include <stdio.h>
-int Partition(int V[], int ini, int fin){
-         int x = V[fin];
-         int i = ini;
-         for(int j = ini; j <= (fin - 1); j++){
-               if ( V[j] <= x){
-                     int aux;
-		     V[i]=aux;
-		     aux=V[j];
-		     V[j]=V[i];
-                     i = i + 1;
-	       }
-	 int aux;
-	 V[i]=aux;
-         aux=V[fin];      
-	 V[i]=V[fin];
-	 } 
-	return i;
-               }
+#include <iostream>
 
-void QuickSort( int V[] ,int ini, int fin){
-        if (ini < fin){
-                 int pivot = Partition(V, ini, fin);
-                 QuickSort(V, ini, pivot - 1);
-                 QuickSort(V, pivot + 1, fin);
+template <typename T>
+void Intercambiar(T& a, T& b) {
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+template <typename T>
+int Partition(T V[], int ini, int fin) {
+    T x = V[fin];
+    int i = ini;
+
+    for (int j = ini; j <= fin - 1; j++) {
+        if (V[j] <= x) {
+            Intercambiar(V[i], V[j]);
+            i = i + 1;
         }
+    }
+
+    Intercambiar(V[i], V[fin]);
+    return i;
+}
+
+template <typename T>
+void QuickSort(T V[], int ini, int fin) {
+    if (ini < fin) {
+        int pivot = Partition(V, ini, fin);
+        QuickSort(V, ini, pivot - 1);
+        QuickSort(V, pivot + 1, fin);
+    }
+}
+
+template <typename T>
+void ImprimirArray(T arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
 }
