98f9a6a8ba1dd24123fecde1c332241059271d85
Baadr66 <145703927+Baadr66@users.noreply.github.com>
2023-11-22 14:41:45 +0100

    test_DyV.cpp

--- test_DyV.cpp
+++ test_DyV.cpp
@@ -1,11 +1,28 @@
-#include <vector>
-#include "DyV.h"
+#include<vector>
 #include <iostream>
-#include <stdio.h>
-using namespace std;
-int main (){
-	std::vector<int> a{1,3,6,9,19,44,55};
-	cout << BusquedaBinaria(44, a,a[0], a[a.size()]);
+#include "DyV.h"
 
+int main(){
+	std::vector<int> a{1,12,3,5};            
+	std::vector<double> b{2.2,3.2,4.4,5.5};      
+	std::vector<char> c{'c', 'f', 'a'};
+	std::vector<int> d{36,12,5,2};
+	std::vector <double>e{10.2,5.4,4.4,2.3};
+	std::vector<char> f{'f','c','a'};
+	std:: vector<int> g{12,6, 4,0};
 
+	std::cout <<  "BusquedaBinaria Int: "<<BusquedaBinaria (12,a,0,3)<<"\n";
+	std::cout << "BusquedaBinaria Double: "<< BusquedaBinaria(4.4,b,0,3)<<"\n";
+	std::cout << "BusquedaBinaria Char: "<< BusquedaBinaria('a',c,0,2)<<"\n";
+	std::cout << "BusquedaBinariaINV int: "<<BusquedaBinariaINV(5,d,0,3)<<"\n";
+	std::cout << "BusquedaBinariaINV Double: " << BusquedaBinariaINV(4.4,e,0,3)<<"\n";
+	std::cout << "BusquedaBinariaINV Char: " <<BusquedaBinariaINV ('c',f,0,2)<<"\n";
+
+	QuickSort(g, 0, 3);
+
+	for(int i = 0; i < 4; i++){
+		std::cout<<g[i]<<" ";
+	}
+
+		std::cout<<std::endl;
 }
